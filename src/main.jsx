fd4a8c995e17e4ab2cd41d31f09fb3e2cdf2ecdb
jaya surya <suryasarisa99.com>
2023-08-06 20:08:36 +0000

    added images & pwa

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App.jsx";
 import "./index.scss";
 import DataProvider from "./context/DataContext.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { registerSW } from "virtual:pwa-register";
+registerSW();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
